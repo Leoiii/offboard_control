944d7a5eccfc40fba0f9817166e38cb369709f2d
Leoiii <lbeukeniii@gmail.com>
2023-02-18 09:42:57 -0700

    commit

--- src/vicon_position_control.cpp
+++ src/vicon_position_control.cpp
@@ -15,14 +15,14 @@ void vicon_cb(const geometry_msgs::PoseStamped &vicon)
 
   //transformed_pose.header = vicon.header;
 
-  transformed_pose.pose.position.x = 0.0;//vicon.pose.position.x; //rotates mocap NUE frame to ENU frame for MAVROS
-  transformed_pose.pose.position.y = 0.0;//-vicon.pose.position.z;
-  transformed_pose.pose.position.z = -0.1;//vicon.pose.position.y;
+  transformed_pose.pose.position.x = vicon.pose.position.x; //rotates mocap NUE frame to ENU frame for MAVROS
+  transformed_pose.pose.position.y = -vicon.pose.position.z;
+  transformed_pose.pose.position.z = vicon.pose.position.y;
 
-  transformed_pose.pose.orientation.x = 0;//vicon.pose.orientation.x; //Q:is it okay to just swap the quaternion components also?  
-  transformed_pose.pose.orientation.y = 0;//-vicon.pose.orientation.z; //A:According to px4 documentation it is
-  transformed_pose.pose.orientation.z = 0;//vicon.pose.orientation.y;
-  transformed_pose.pose.orientation.w = 1;//vicon.pose.orientation.w;
+  transformed_pose.pose.orientation.x = vicon.pose.orientation.x; //Q:is it okay to just swap the quaternion components also?  
+  transformed_pose.pose.orientation.y = -vicon.pose.orientation.z; //A:According to px4 documentation it is
+  transformed_pose.pose.orientation.z = vicon.pose.orientation.y;
+  transformed_pose.pose.orientation.w = vicon.pose.orientation.w;
 
   mavros_pose_pub.publish(transformed_pose);
 }
@@ -32,7 +32,7 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "vicon_node");
   ros::NodeHandle nh;
   mavros_pose_pub = nh.advertise<geometry_msgs::PoseStamped>("/mavros/vision_pose/pose",2);
-  vicon_sub = nh.subscribe("/vrpn_client_node/act_top/pose",2,vicon_cb);  
+  vicon_sub = nh.subscribe("/vrpn_client_node/drone/pose",2,vicon_cb);  
 
   geometry_msgs::PoseStamped setpoint;
   int count = 1;
